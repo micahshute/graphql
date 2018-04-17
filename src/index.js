8f80c74fe94231f3c60b0c60901f19c117e876a3
Micah Shute <micah.shute@gmail.com>
2018-04-17 17:26:45 -0400

    Connected graphql server to prisma db, starting authentication

--- src/index.js
+++ src/index.js
@@ -1,51 +1,41 @@
 
+//prisma 1.6.2
 const { GraphQLServer } = require('graphql-yoga');
-
-let votes = [
-    {
-        id: '0',
-        type: 'upvote',
-        startIndex: 0,
-        endIndex: 5
-
-    }
-]
-
-
-
-const typeDefs = `
-    type Query{
-        info: String!
-        myVotes: [Vote!]!
-    }
-
-
-    type Vote {
-        id: ID!
-        type: String!
-        startIndex: Int!
-        endIndex: Int!
-    }
-`;
+const { Prisma } = require('prisma-binding');
 
 const resolvers = {
     Query: {
         info: () => `This is a test HHSB server`,
-        myVotes: () => votes,
+        myVotes: () => (root, args, context, info) => {
+            return context.db.query.votes({}, info);  
+        }
     },
 
-    Vote: {
-        id: (root) => root.id,
-        type: (root) => root.type,
-        startIndex: (root) => root.startIndex,
-        endIndex: (root) => root.endIndex
+    Mutation: {
+        vote: (root, args, context, info) => {
+            const vote = {
+                type: args.type,
+                startIndex: args.startIndex,
+                endIndex: args.endIndex
+            }
+            return context.db.mutation.createVote({ data: vote }, info)
+        },
     }
 }
 
 
 const server = new GraphQLServer({
-    typeDefs,
+    typeDefs: './src/schema.graphql',
     resolvers,
+    context: req => ({
+        ...req,
+        db: new Prisma({
+            typeDefs: 'src/generated/prisma.graphql',
+            endpoint: 'https://us1.prisma.sh/public-coralwarlock-759/scoreboard-node/dev',
+            secret: 'seal90210',
+            debug: true,
+        })
+    })
 });
 
 
