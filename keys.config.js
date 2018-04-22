7be7e55e3bca3b94cbd695c630b6824860ce2bac
Micah Shute <micah.shute@gmail.com>
2018-04-22 14:38:45 -0400

    making secure

--- keys.config.js
+++
@@ -1,7 +0,0 @@
-const GENIUS_TOKEN = 'UJYTnEMS0QZluVoY_1yooeL3OHXnsv9ShkAVK48GKjCJvvN4zYVrR60FJwnrnLeb';
-const APP_SECRET = 'This-is-my-secret-t0k3n';
-
-module.exports = {
-    GENIUS_TOKEN,
-    APP_SECRET
-};
